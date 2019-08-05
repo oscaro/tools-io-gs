5b88fb52cb4242576f981a4c7fc64294db098b74
Baptiste Fontaine <baptiste.fontaine@oscaro.com>
2019-08-05 13:51:34 +0200

    Add CI config

--- project.clj
+++ project.clj
@@ -4,8 +4,25 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.oscaro/tools-io "0.3.16"]
+                 [com.oscaro/tools-io "0.3.17"]
                  [com.oscaro/clj-gcloud-storage "0.71-1.2"]]
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["shell" "git" "commit" "-am" "Version ${:version} [ci skip]"]
+                  ["vcs" "tag" "v" "--no-sign"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["shell" "git" "commit" "-am" "Version ${:version} [ci skip]"]
+                  ["vcs" "push"]]
+  :plugins [[lein-shell "0.5.0"]]
   :profiles {:dev {:global-vars    {*warn-on-reflection* true}
                    :plugins        [[lein-codox "0.10.2"]]
                    :dependencies   [[org.clojure/tools.namespace "0.2.11"]]}})
