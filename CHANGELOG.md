98785938eee73224a1fe2b4877023c2471544398
Ed Porras <dev@digressed.net>
2021-08-19 10:02:08 +0200

    bump dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
-# oscaro-tools-io-gs Changelog
+# oscaro/tools-io-gs Changelog
 
-## 0.1.5 (2019/05/16)
+## [Unreleased]
+* bump clojure, tools-io, clj-gcloud-storage, tools.namespace
 
-First public release.
+## [0.1.5] - 2019-05-16
+* First public release.
+
+[Unreleased]: https://github.com/oscaro/tools-io-gs/-/compare/0.1.5...master
+[0.1.5]: https://github.com/oscaro/tools-io-gs/releases/tag/0.1.5
--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/oscaro/tools-io-gs"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.oscaro/tools-io "0.3.16"]
-                 [com.oscaro/clj-gcloud-storage "0.71-1.2"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [com.oscaro/clj-gcloud-storage "0.159-1.0"]
+                 [com.oscaro/tools-io "0.3.22"]]
   :profiles {:dev {:global-vars    {*warn-on-reflection* true}
                    :plugins        [[lein-codox "0.10.2"]]
-                   :dependencies   [[org.clojure/tools.namespace "0.2.11"]]}})
+                   :dependencies   [[org.clojure/tools.namespace "1.1.0"]]}})
