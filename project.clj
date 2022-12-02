2efcfe2bb5823318d632ccceed115aa4935cf99a
Ed Porras <dev@digressed.net>
2022-12-02 13:42:54 +0100

    Version 0.1.11-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/tools-io-gs "0.1.10"
+(defproject com.oscaro/tools-io-gs "0.1.11-SNAPSHOT"
   :description "google storage support for tools-io"
   :url "https://github.com/oscaro/tools-io-gs"
   :license {:name "Eclipse Public License"
