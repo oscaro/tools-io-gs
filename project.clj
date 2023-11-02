71a3015ab37927a67c54d78099a4723aee343f0a
iomonad <iomonad@riseup.net>
2023-11-02 17:22:40 +0100

    bump: 0.1.15-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/tools-io-gs "0.1.14-SNAPSHOT"
+(defproject com.oscaro/tools-io-gs "0.1.15-SNAPSHOT"
   :description "google storage support for tools-io"
   :url "https://github.com/oscaro/tools-io-gs"
   :license {:name "Eclipse Public License"
