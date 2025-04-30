63595a29b50983bacb8e95e73494d461603c3e0e
iomonad <iomonad@riseup.net>
2025-04-30 17:54:31 +0200

    release: 0.1.20

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.oscaro/tools-io-gs "0.1.20-SNAPSHOT"
+(defproject com.oscaro/tools-io-gs "0.1.20"
   :description "google storage support for tools-io"
   :url "https://github.com/oscaro/tools-io-gs"
   :license {:name "Eclipse Public License"
